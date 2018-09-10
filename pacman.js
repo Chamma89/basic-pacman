57398df5a580b7a1294dc6b8a6a63e37136af120
fouad <Chamma.fouad@gmail.com>
2018-09-10 10:25:13 +1000

    added starting function that receives input from user

---
+++ pacman.js
@@ -0,0 +1,16 @@
+var xCoor = 0
+var yCoor = 0
+var direction = ""
+
+function letsPlay(){
+  xCoor = 0
+  yCoor = 0
+  direction = ""
+
+  direction = prompt("Please enter starting direction. Eg east")
+  xCoor = Number(prompt("Please enter X coordinate"))
+  yCoor = Number(prompt("Please enter Y coordinate"))
+
+}
+
+
